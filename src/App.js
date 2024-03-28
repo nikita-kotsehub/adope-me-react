af4b060ab42d20d4fd8171c18e68e4c79f79feb3
Nikita Kotsehub <nikita@nikitas-mbp.mynetworksettings.com>
2024-03-27 22:09:54 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+const Pet = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed),
+    ])
+}
+
+const App = () => {
+    return React.createElement(
+        "div",
+        {},
+        [
+            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet, { name: "Zara", animal: "dog", breed: "best dog" }),
+            React.createElement(Pet, { name: "Milka", animal: "cat", breed: "kittie" }),
+            React.createElement(Pet, { name: "Marsik", animal: "cat", breed: "cute cat!" }),
+        ]
+    )
+}
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
